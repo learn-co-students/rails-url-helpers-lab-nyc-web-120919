88539e2eab3ef6aa5a4b9e24ff6d0a8dad53d2a5
github username <github email address>
2020-01-09 10:08:48 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def activate 
+    self.active = !self.active?
+    self.save
+  end
+
+  def active_to_s
+    self.active ? "active" : "inactive"
+  end
 end
